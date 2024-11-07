daf61cc24348b87d7636167d16740cbe9806acb2
Vivek5501 <2003viv10@gmail.com>
2024-11-07 12:30:26 +0530

    done

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",],
+  theme: {
+    extend: {
+      colors: {
+        pastelPink: '#FDE2E4',
+        pastelLavender: '#DAD7FF',
+        pastelMint: '#B8E8D3',
+        pastelBlue: '#D0E2FF',
+      },
+    },
+  },
+  plugins: [],
+}
+
